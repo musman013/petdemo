810b10c409b8ee88820fb3fb105c803dfe11da6a
fastCode <info@nfinityllc.com>
2020-06-24 17:06:42 +0500

    get permalink by resource added

--- demo/src/main/java/com/fastcode/demopet/domain/irepository/IPermalinkRepository.java
+++ demo/src/main/java/com/fastcode/demopet/domain/irepository/IPermalinkRepository.java
@@ -1,6 +1,7 @@
 package com.fastcode.demopet.domain.irepository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +10,8 @@ import java.util.UUID;
 
 @Repository
 public interface IPermalinkRepository extends JpaRepository<PermalinkEntity, UUID>,QuerydslPredicateExecutor<PermalinkEntity> {
-
+	
+	@Query("select r from PermalinkEntity r where r.resourceId = ?1 and r.resource = ?2")
+	PermalinkEntity findByResourceIdAndResource(Long resourceId, String resource);
+	
 }
