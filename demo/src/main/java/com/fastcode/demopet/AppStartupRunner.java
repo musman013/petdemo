35817e7dbd4b5ce5addeb10652c73de80e74d90c
fastCode <info@nfinityllc.com>
2020-06-30 13:35:36 +0500

    visit permission added for owner

--- demo/src/main/java/com/fastcode/demopet/AppStartupRunner.java
+++ demo/src/main/java/com/fastcode/demopet/AppStartupRunner.java
@@ -119,9 +119,9 @@ public class AppStartupRunner implements ApplicationRunner {
 		}
 		
 		
-//		assignEntityPermissions("pets", role1);
+		assignEntityPermissions("visits", role1);
 		assignEntityReadPermission("invoices", role1);
-		assignEntityReadPermission("visits", role1);
+//		assignEntityReadPermission("visits", role1);
 //		assignEntityReadPermission("invoices", role2);
 		assignEntityReadPermission("visits", role2);
 		
