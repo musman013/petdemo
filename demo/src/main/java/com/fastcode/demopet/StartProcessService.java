790980b9e68d8fffc110999cbf2313697d97f252
fastCode <info@nfinityllc.com>
2020-07-14 16:14:49 +0500

    @Lazyy added on appService in startprocess

--- demo/src/main/java/com/fastcode/demopet/StartProcessService.java
+++ demo/src/main/java/com/fastcode/demopet/StartProcessService.java
@@ -7,6 +7,7 @@ import org.flowable.engine.TaskService;
 import org.flowable.engine.runtime.ProcessInstance;
 import org.flowable.task.service.delegate.DelegateTask;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -29,6 +30,7 @@ public class StartProcessService {
 	private RuntimeService runtimeService;
 
 	@Autowired
+	@Lazy
 	private IInvoicesAppService _invoicesAppService;
 
 	@Autowired
